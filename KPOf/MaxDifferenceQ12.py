dfa99b0e56d17bb7ec50dd0d8b348cc17efcada2
Rajesh <rajeshkumarraje9@gmail.com>
2022-06-13 20:36:10 +0530

    codes updated

--- KPOf/MaxDifferenceQ12.py
+++ KPOf/MaxDifferenceQ12.py
@@ -1,12 +1,28 @@
-def maxDifference(l):
-    diffList = []
-    for i in range(0,len(l)):
-        for j in range(i+1, len(l)):
-            diffList.append(l[j]-l[i])
-    return max(diffList)
+#Time complexity is O(N)
+
+def maxDiff(a):
+    vmin = a[0]
+    dmax = 0
+    for i in range(len(a)):
+        if (a[i] < vmin):
+            vmin = a[i]
+        elif (a[i] - vmin > dmax):
+            dmax = a[i] - vmin
+    return dmax
 l = [1,2,6,4]
-res=maxDifference(l)
-print(res)
+print(maxDiff(l))
+
+
+#Time complexity is O(N^2)
+#  def maxDifference(l):
+#     diffList = []
+#     for i in range(0,len(l)):
+#         for j in range(i+1, len(l)):
+#             diffList.append(l[j]-l[i])
+#     return max(diffList)
+# l = [1,2,6,4]
+# res=maxDifference(l)
+# print(res)
 
 # def maxDifference(l):
 #     res = l[1] - l[0]
