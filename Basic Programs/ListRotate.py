1f7a83d1ee16cd45f8ac38e52396e35429d88484
Rajesh <rajeshkumarraje9@gmail.com>
2024-09-14 20:22:06 +0530

    Updating the list rotate code

--- Basic Programs/ListRotate.py
+++ Basic Programs/ListRotate.py
@@ -1,8 +1,85 @@
-for i in range(int(input())):
-    str=list(map(int,input().split()))
-    num=str[1]
-    lst=list(input().split())[0:str[0]]
-    s=num%len(lst)
-    p=len(lst)-s
-    k=lst[p:]+lst[0:p]
-    print(' '.join(k))
+# Number of test cases
+for test_case in range(int(input())):
+    
+    # Read input and split into two integers
+    size, num_rotations = map(int, input().split())
+    
+    # Read the list of elements and take only the first 'size' elements
+    elements = list(input().split())[:size]
+    
+    # Calculate the number of effective rotations needed
+    effective_rotations = num_rotations % len(elements)
+    
+    # Calculate the split point for the rotation
+    split_point = len(elements) - effective_rotations
+    
+    # Perform the rotation by splitting and rearranging the list
+    rotated_list = elements[split_point:] + elements[:split_point]
+    
+    # Print the rotated list as a space-separated string
+    print(' '.join(rotated_list))
+
+
+'''
+Here are a few sample inputs and their corresponding expected outputs to test the code:
+
+### Sample Input 1:
+```
+1
+5 2
+1 2 3 4 5
+```
+
+### Expected Output 1:
+```
+4 5 1 2 3
+```
+
+### Explanation:
+- You have 5 elements: `1 2 3 4 5`.
+- You need to rotate the list by 2 positions.
+- The result after 2 right rotations is: `4 5 1 2 3`.
+
+---
+
+### Sample Input 2:
+```
+2
+6 3
+10 20 30 40 50 60
+4 8
+1 2 3 4
+```
+
+### Expected Output 2:
+```
+40 50 60 10 20 30
+1 2 3 4
+```
+
+### Explanation:
+- **First test case:** You have 6 elements: `10 20 30 40 50 60` and rotate by 3 positions, giving: `40 50 60 10 20 30`.
+- **Second test case:** You have 4 elements: `1 2 3 4`, rotating by 8 is the same as rotating by 0 because 8 is a multiple of 4, so the result is `1 2 3 4`.
+
+---
+
+### Sample Input 3:
+```
+1
+7 5
+a b c d e f g
+```
+
+### Expected Output 3:
+```
+c d e f g a b
+```
+
+### Explanation:
+- You have 7 elements: `a b c d e f g`.
+- Rotate by 5 positions, resulting in: `c d e f g a b`.
+
+This will help you verify that the rotation logic and input parsing are correct.
+
+'''
+
---
+++ Basic Programs/Longest_Common_Prefix.py
@@ -0,0 +1,18 @@
+# Find the longest common prefix
+
+l = ["flower", "flow", "flight"]
+
+def longest_common_prefix(strs):
+    if not strs:
+        return ""
+    prefix = strs[0]
+    for s in strs[1:]:
+        while not s.startswith(prefix):
+            prefix = prefix[:-1]
+            if not prefix:
+                return ""
+        
+    return prefix
+
+
+print(longest_common_prefix(l)) 
