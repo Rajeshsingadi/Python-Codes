e77fa00cb212bd9ea1cd1de653dd1fb19ca72c7f
Rajesh <rajeshkumarraje9@gmail.com>
2024-09-16 20:39:54 +0530

    updated the code for the most common element in the list

--- Basic Programs/max-1.py
+++
@@ -1,4 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    arr = set(list(map(int, input().split())))
-    print(arr)
--- Basic Programs/mostCommonEle.py
+++ Basic Programs/mostCommonEle.py
@@ -1,7 +1,28 @@
-from collections import Counter
+l = [10, 10, 10, 5, 5, 2, 9, 9, 9, 9, 9]
+frequency = {}
+for num in l:
+    if num in frequency:
+        frequency[num] += 1
+    else:
+        frequency[num]= 1
+print(frequency)
 
-l = [10,10,10,5,5,2,9,9,9,9,9]
-counter = Counter(l)
-# print(counter)
-most_common = counter.most_common(2)
-print(most_common[0][0])
+max_val = max(frequency, key=frequency.get)
+
+print("Maximum number of times that is repeated in the list is : ", max_val)
+
+print("Number of times the maximum value is repeated is: ", frequency[max_val])
+
+'''
+Time Complexity:
+Building the frequency dictionary: O(n), where n is the length of the list l.
+Finding the maximum: max() iterates over all k elements in the dictionary (where k is the number of unique elements). The key=frequency.get ensures that the maximum is found based on the value (frequency).
+This step takes O(k) time.
+In the worst case, k = n.
+Total time complexity: O(n) + O(k) ≈ O(n) in the worst case.
+
+Space Complexity:
+Dictionary: O(k), where k is the number of unique elements in the list.
+Total space complexity: O(k), which is O(n) in the worst case when all elements are distinct.
+
+'''
